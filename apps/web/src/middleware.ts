851512ca4da68ba01e21b62313c50ec20026cd8a
Alvin Odhi N <ucokkocu411@gmail.com>
2025-02-28 01:48:08 +0700

    feat: authorization on client

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,12 +1,65 @@
-// export { auth as middleware } from '@/auth';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-import { NextRequest, NextResponse } from 'next/server';
+export async function middleware(req: NextRequest) {
+  try {
+    const apiUrl = process.env.NEXT_PUBLIC_BACKEND_URL + '/api/users/identity';
 
-// This function can be marked `async` if using `await` inside
-export default function middleware(request: NextRequest) {
-  return NextResponse.next();
+    const res = await fetch(apiUrl, {
+      headers: { cookie: req.headers.get('cookie') || '' }, // Forward cookies
+      credentials: 'include', // Allow cookies in cross-origin requests
+    });
+
+    const requestPath = req.nextUrl.pathname;
+
+    // 🔹 If user is NOT logged in
+    if (!res.ok) {
+      if (
+        requestPath.startsWith('/members') &&
+        !requestPath.startsWith('/members/auth')
+      ) {
+        return NextResponse.redirect(new URL('/members/auth/signin', req.url));
+      }
+
+      if (requestPath.startsWith('/payment')) {
+        return NextResponse.rewrite(new URL('/404', req.url)); // Show 404 page
+      }
+
+      return NextResponse.next(); // other path is ok;
+    }
+
+    const { user } = (await res.json()) as {
+      user: { role: 'CUSTOMER' | 'ORGANIZER' };
+    };
+
+    const userIsCustomer = user.role === 'CUSTOMER';
+    const userIsOrganizer = user.role === 'ORGANIZER';
+
+    // 🔹 If a CUSTOMER tries to access `/members/*` (except `/members/auth`)
+    if (
+      userIsCustomer &&
+      requestPath.startsWith('/members') &&
+      !requestPath.startsWith('/members/auth')
+    ) {
+      return NextResponse.redirect(new URL('/members/auth/signin', req.url));
+    }
+
+    // 🔹 If an ORGANIZER tries to access `/payment/*`
+    if (userIsOrganizer && requestPath.startsWith('/payment')) {
+      return NextResponse.rewrite(new URL('/404', req.url)); // Show 404 page
+    }
+
+    return NextResponse.next(); // Proceed if authorized
+  } catch (error) {
+    console.log({ error });
+    throw error;
+  }
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  ],
 };
